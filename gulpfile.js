232badb158bce24f4a3a497590155951ca1aeda4
György Homonnai <homongy@gmail.com>
2022-02-26 00:49:00 +0100

    remove gulp copy import

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const {src, dest, watch} = require('gulp');
 const compileSass = require('gulp-sass')(require('sass'));
 const minifyCss = require('gulp-clean-css');
 const concat = require('gulp-concat');
-const gulpCopy = require('gulp-copy');
 
 const bundleSass = () => {
     return src('./static/scss/**/*.scss')
