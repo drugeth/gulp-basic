5de95004a4dc0fe97f17bc0b2b290a877cc9bd30
György Homonnai <homongy@gmail.com>
2022-02-28 10:44:42 +0100

    Modify config

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,51 @@ const {src, dest, watch} = require('gulp');
 const compileSass = require('gulp-sass')(require('sass'));
 const minifyCss = require('gulp-clean-css');
 const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const merge = require('merge-stream');
 
-const bundleSass = () => {
-    return src('./static/scss/**/*.scss')
-    .pipe(compileSass().on('error', compileSass.logError))
-    .pipe(minifyCss())
-    .pipe(concat('bundle.css'))
-    .pipe(dest('./dist/css/'))
+const bundle = () => {
+    return bundleJs()
+    .pipe(bundleSass())
     .pipe(copyStaticFiles());
 }
 
+const bundleSass = () => {
+    return src('static/scss/main.scss')
+    .pipe(compileSass().on('error', compileSass.logError))
+    .pipe(minifyCss())
+    .pipe(concat('bundle.css'))
+    .pipe(dest('dist/css/'));
+}
+
+const bundleJs = () => {
+    return src('static/js/**/*.js')
+    .pipe(babel({
+        presets: ['@babel/preset-env']
+    }))
+    .pipe(concat('bundle.js'))
+    .pipe(dest('dist/js'))
+}
+
 const copyStaticFiles = () => {
-    return src('./static/html/*')
-    .pipe(dest('./dist'));
+    const folders = [
+        {src: 'static/html/*', dest: 'dist'},
+        {src: 'static/assets/*', dest: 'dist/assets'}
+    ];
+
+    let tasks = folders.map((el) => {
+        return src(el.src)
+        .pipe(dest(el.dest));
+    })
+
+    return merge(tasks);
 }
 
 const devWatch = () => {
-    watch('./static/scss/**/*.scss', bundleSass);
+    watch('static/**/*', bundleSass);
 }
 
+exports.bundle = bundle;
 exports.bundleSass = bundleSass;
 exports.devWatch = devWatch;
+exports.bundleJs = bundleJs;
---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+class App {
+    constructor() {
+        console.log('APP CREATED');
+    };
+}
+
+const app = new App();
